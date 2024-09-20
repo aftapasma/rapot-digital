06033f53bbc163b8f1628e03a7fc45ebe5ffc261
Afta Pasma Lohdri <aftaplasma708@gmail.com>
2024-09-20 10:35:37 +0700

    membuat componen top dan sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
